039b17caf16cde14e2fe894442904cde7b70ddea
dustgard <77804262+dustgard@users.noreply.github.com>
2023-01-22 19:24:09 -0700

    yeah

--- src/main/java/edu/carroll/cs389/web/form/LoginForm.java
+++ src/main/java/edu/carroll/cs389/web/form/LoginForm.java
@@ -1,8 +1,29 @@
 package edu.carroll.cs389.web.form;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class LoginForm {
+    @NotNull
+    @Size(min = 6, message = "Username must be at least 6 characters long")
     private String username;
+
+    @NotNull
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
+
+    @PostMapping("/login")
+    public String loginPost(@Valid @ModelAttribute LoginForm loginForm, BindingResult result) {
+        System.out.println("User '" + loginForm.getUsername() + "' attempted login");
+        if (result.hasErrors()) {
+            return "login";
+        }
+        return "redirect:/loginSuccess";
+    }
 
     public String getUsername() {
         return username;
